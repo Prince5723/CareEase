c55f6947a8d6e5d10687854d89fa2d099803c073
Prince5723 <57prince23@gmail.com>
2025-06-22 18:36:15 +0530

    reconfiguration of index

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser())
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://prashant:prashant@cluster0.zktg0fy.mongodb.net/', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
